73a87ed6e7c06f502d16f88e6808f4647ccf2bfb
malikla <laszlo.malik@gmail.com>
2022-03-01 23:37:14 +0100

    reformat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
-import Cart from "./components/Cart/Cart"
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-  return (
-    <CartProvider>
-      <Cart/>
-      <Header />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
-  );
+    return (
+        <CartProvider>
+            <Cart />
+            <Header />
+            <main>
+                <Meals />
+            </main>
+        </CartProvider>
+    );
 }
 
 export default App;
